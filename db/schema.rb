6f06b86df248a76e973a5bd49a88fe253b2a1bc1
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-05-02 07:10:06 +0530

    Add bio to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_02_011920) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_02_013945) do
   create_table "microposts", force: :cascade do |t|
     t.string "content"
     t.integer "next_post"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_02_011920) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "bio"
   end
 
 end
