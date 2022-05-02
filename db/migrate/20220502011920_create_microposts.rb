4111bcc2ee1e676a1f39795dd0218ae638596a8a
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-05-02 06:49:33 +0530

    Add model attributes

--- db/migrate/20220502011920_create_microposts.rb
+++ db/migrate/20220502011920_create_microposts.rb
@@ -1,6 +1,8 @@
 class CreateMicroposts < ActiveRecord::Migration[7.0]
   def change
     create_table :microposts do |t|
+      t.string :content
+      t.integer :next_post
 
       t.timestamps
     end
